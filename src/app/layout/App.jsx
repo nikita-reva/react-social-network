b835984da0dbf81dc290dde51c8dea748954db51
nikita-reva <68555738+nikita-reva@users.noreply.github.com>
2021-02-07 14:15:55 +0100

    Navbar created

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard'
+import NavBar from '../../features/nav/NavBar'
 
-function App() {
+export default function App() {
+	/* Without JSX */
+	// const title = React.createElement('h1', {}, 'REvents without JSX')
+	// const div = React.createElement('div', { className: 'App' }, title)
+
 	return (
-		<div className='App'>
-			<h1>REvents</h1>
-		</div>
+		<Fragment>
+			<NavBar />
+			<Container className="main">
+				<EventDashboard />
+			</Container>
+		</Fragment>
 	)
 }
-
-export default App
