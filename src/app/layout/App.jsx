38906e9b79b2235bff5f5093a5634364d6fe65e7
nikita-reva <68555738+nikita-reva@users.noreply.github.com>
2021-03-07 15:59:04 +0100

    CRUD operations added

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -9,12 +9,28 @@ export default function App() {
 	// const div = React.createElement('div', { className: 'App' }, title)
 
 	const [formOpen, setFormOpen] = useState(false)
+	const [selectedEvent, setSelectedEvent] = useState(null)
+
+	function handleSelectEvent(event) {
+		setSelectedEvent(event)
+		setFormOpen(true)
+	}
+
+	function handleCreateFormOpen() {
+		setSelectedEvent(null)
+		setFormOpen(true)
+	}
 
 	return (
 		<Fragment>
-			<NavBar setFormOpen={setFormOpen} />
+			<NavBar setFormOpen={handleCreateFormOpen} />
 			<Container className="main">
-				<EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+				<EventDashboard
+					formOpen={formOpen}
+					setFormOpen={setFormOpen}
+					selectEvent={handleSelectEvent}
+					selectedEvent={selectedEvent}
+				/>
 			</Container>
 		</Fragment>
 	)
--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,28 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Header, Segment, Form, Button } from 'semantic-ui-react'
+import cuid from 'cuid'
 
-export default function EventForm({ setFormOpen }) {
+export default function EventForm({
+	setFormOpen,
+	setEvents,
+	createEvent,
+	updateEvent,
+	selectedEvent,
+}) {
+	const initialValues = selectedEvent ?? {
+		title: '',
+		category: '',
+		description: '',
+		city: '',
+		venue: '',
+		date: '',
+		attendees: [],
+	}
+
+	const [values, setValues] = useState(initialValues)
+
+	function handleInputChange(e) {
+		const { name, value } = e.target
+		setValues({ ...values, [name]: value })
+	}
+
+	function handleFormSubmit() {
+		selectedEvent
+			? updateEvent({ ...selectedEvent, ...values })
+			: createEvent({
+					...values,
+					id: cuid(),
+					hostedBy: 'Bob',
+					attendees: [],
+					hostPhotoURL: '/assets/user.png',
+			  })
+		setFormOpen(false)
+	}
+
 	return (
 		<Segment clearing>
-			<Header content="Create new event" />
-			<Form>
+			<Header
+				content={selectedEvent ? 'Edit the event' : 'Create new event'}
+			/>
+			<Form onSubmit={handleFormSubmit}>
 				<Form.Field>
-					<input type="text" placeholder="Event title" />
+					<input
+						type="text"
+						placeholder="Event title"
+						name="title"
+						value={values.title}
+						onChange={(e) => handleInputChange(e)}
+					/>
 				</Form.Field>
 				<Form.Field>
-					<input type="text" placeholder="Category" />
+					<input
+						type="text"
+						placeholder="Category"
+						name="category"
+						value={values.category}
+						onChange={(e) => handleInputChange(e)}
+					/>
 				</Form.Field>
 				<Form.Field>
-					<input type="text" placeholder="Description" />
+					<input
+						type="text"
+						placeholder="Description"
+						name="description"
+						value={values.description}
+						onChange={(e) => handleInputChange(e)}
+					/>
 				</Form.Field>
 				<Form.Field>
-					<input type="text" placeholder="City" />
+					<input
+						type="text"
+						placeholder="City"
+						name="city"
+						value={values.city}
+						onChange={(e) => handleInputChange(e)}
+					/>
 				</Form.Field>
 				<Form.Field>
-					<input type="text" placeholder="Venue" />
+					<input
+						type="text"
+						placeholder="Venue"
+						name="venue"
+						value={values.venue}
+						onChange={(e) => handleInputChange(e)}
+					/>
 				</Form.Field>
 				<Form.Field>
-					<input type="date" placeholder="Date" />
+					<input
+						type="date"
+						placeholder="Date"
+						name="date"
+						value={values.date}
+						onChange={(e) => handleInputChange(e)}
+					/>
 				</Form.Field>
 				<Button
 					type="submit"
