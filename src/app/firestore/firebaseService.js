487a462664f7fa591e8007076bf7d1274a71c181
nikita-reva <68555738+nikita-reva@users.noreply.github.com>
2021-04-14 16:29:07 +0200

    another commit

--- src/app/firestore/firebaseService.js
+++ src/app/firestore/firebaseService.js
@@ -1,4 +1,7 @@
+import { toast } from 'react-toastify'
+
 import firebase from '../config/firebase'
+import { setUserProfileData } from './firestoreService'
 
 export function signInWithEmail(creds) {
 	return firebase
@@ -15,10 +18,31 @@ export async function registerInFirebase(creds) {
 		const result = await firebase
 			.auth()
 			.createUserWithEmailAndPassword(creds.email, creds.password)
-		return await result.user.updateProfile({
+		await result.user.updateProfile({
 			displayName: creds.displayName,
 		})
+		return await setUserProfileData(result.user)
 	} catch (error) {
 		throw error
+	}
+}
+
+export async function socialLogin(selectedProvider) {
+	let provider
+	if (selectedProvider === 'facebook') {
+		provider = new firebase.auth.FacebookAuthProvider()
+	}
+	if (selectedProvider === 'google') {
+		provider = new firebase.auth.GoogleAuthProvider()
+	}
+
+	try {
+		const result = await firebase.auth().signInWithPopup(provider)
+		console.log(result)
+		if (result.additionalUserInfo.isNewUser) {
+			await setUserProfileData(result.user)
+		}
+	} catch (error) {
+		toast.error(error.message)
 	}
 }
